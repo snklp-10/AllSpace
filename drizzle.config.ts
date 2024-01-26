bac2111f1f579c289bc02cf3264cd242d7c124b7
Sankalp <sankalpppppp@gmail.com>
2024-01-26 11:12:08 +0530

    Code updated with Landing page and necessary files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,14 +3,14 @@ import * as dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
 if (!process.env.DATABASE_URL) {
-    console.log("🔴 Cannot find database url");
+  console.log("🔴 Cannot find database url");
 }
 
 export default {
-    schema: "./src/lib/supabase/schema.ts",
-    out: "./migrations",
-    driver: "pg",
-    dbCredentials: {
-        connectionString: process.env.DATABASE_URL || "",
-    },
+  schema: "./src/lib/supabase/schema.ts",
+  out: "./migrations",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL || "",
+  },
 } satisfies Config;
