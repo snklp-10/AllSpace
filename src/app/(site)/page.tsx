a87b2081ee78a33ca7f72a10bb0d8a21810cbd81
Sankalp <sankalpppppp@gmail.com>
2024-01-28 12:48:05 +0530

    Addded components

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,5 +1,6 @@
 import TitleSection from "@/components/landing-page/title-section";
 import React from "react";
+import { Button } from '@/components/ui/button';
 
 const HomePage = () => {
   return (
@@ -16,9 +17,31 @@ const HomePage = () => {
             md:items-center"
       >
         <TitleSection
-          pill= "✨ Your Workspace, Perfected"
-          title= "Productivity, it's like a game"
+          pill="✨ Your Workspace, Perfected"
+          title="Productivity, it's like a game"
         />
+        <div
+          className="bg-white
+        p-[2px]
+        mt-6
+        rounded-xl
+        bg-gradient-to-r
+        from-primary
+        to-brand/brand-primaryBlue
+        sm:w-[300px]
+        "
+        >
+          <Button
+            variant="btn-secondary"
+            className="w-full
+            rounded-[10px]
+            p-6
+            text-2xl
+            bg-background"
+          >
+            AllSpace
+          </Button>
+        </div>
       </div>
     </section>
   );
